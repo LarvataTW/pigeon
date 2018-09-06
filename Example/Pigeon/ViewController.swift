2fde71f5c7505f8163ddc598677ab522d0ceea59
Joe Jiang <happiness9721@gmail.com>
2018-09-06 15:49:12 +0800

    chore: Create Pod Library Structure

---
+++ Example/Pigeon/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Pigeon
+//
+//  Created by Larvata on 09/06/2018.
+//  Copyright (c) 2018 Larvata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+}
+
